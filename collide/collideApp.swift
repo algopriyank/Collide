29e732d1ec2380d2757adce169b60225e5526d4b
Priyank Sharma <algo.priyank@gmail.com>
2025-04-07 14:37:27 +0530

    Initial Commit

---
+++ collide/collideApp.swift
@@ -0,0 +1,17 @@
+//
+//  collideApp.swift
+//  collide
+//
+//  Created by Priyank Sharma on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct collideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
